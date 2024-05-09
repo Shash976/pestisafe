9976304479d917083aeb4baf63f68252f3826fae
Shashwat Goel <itshashgoel@gmail.com>
2024-05-09 10:02:06 +0530

    Working Calib screen with few UI errors

--- app/src/main/java/com/example/wifigetdata/CalibrationActivity.kt
+++ app/src/main/java/com/example/wifigetdata/CalibrationActivity.kt
@@ -1,10 +1,13 @@
 package com.example.wifigetdata
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.collection.mutableFloatFloatMapOf
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -12,23 +15,27 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import com.example.wifigetdata.ui.theme.WifigetdataTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -50,18 +57,27 @@ class CalibrationActivity : ComponentActivity() {
 
     @SuppressLint("CoroutineCreationDuringComposition")
     @Composable
-    fun CalibrationScreen(){
-        val counter  = remember { mutableIntStateOf(0) }
+    fun CalibrationScreen() {
+        val counter = remember { mutableIntStateOf(0) }
 
         @Composable
-        fun CalibrateCard(index :Int){
+        fun CalibrateCard(index: Int) {
             val (labelText, setLabelText) = remember { mutableDoubleStateOf(0.0) }
             val concentration = BasicValues.getCalibrationConcentration()[index]
-            val textFixed by remember(counter){ derivedStateOf { counter.intValue !=index }}
+            val textFixed by remember(counter) { derivedStateOf { counter.intValue != index } }
 
-            Card (modifier = Modifier.padding(10.dp), shape=CardDefaults.elevatedShape, elevation = CardDefaults.cardElevation(defaultElevation = 6.dp )){
-                Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.padding(10.dp)){
+            Card(
+                modifier = Modifier.padding(10.dp),
+                shape = CardDefaults.elevatedShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(10.dp)
+                ) {
                     val coroutineScope = rememberCoroutineScope()
+                    val mathCoroutineScope = rememberCoroutineScope()
                     coroutineScope.launch {
                         withContext(Dispatchers.IO) {
                             while (BasicValues.getURL().isNotEmpty() && !textFixed) {
@@ -73,16 +89,29 @@ class CalibrationActivity : ComponentActivity() {
                     }
                     Text(text = "$labelText V")
                     Text(text = "$concentration μm")
-                    Button(shape= RoundedCornerShape(10.dp), enabled = !textFixed, onClick = {
+                    Button(shape = RoundedCornerShape(10.dp), enabled = !textFixed, onClick = {
                         println("Counter ${counter.intValue}")
-                        val newDataValue = DataValue(0, labelText, concentration)
+                        //val newDataValue = DataValue(0, labelText, concentration)
                         //database.dataValueDao().insert(newDataValue)
-                        BasicValues.updateData(newDataValue.voltage, newDataValue.concentration)
-                        counter.intValue += 1
+                        BasicValues.updateData(labelText, concentration)
+                        println("$labelText, $concentration")
                         if (BasicValues.getConcentrationDataArray().size >= 2) {
-                            updateR2Score()
-                            updateGradientIntercept()
+                            mathCoroutineScope.launch {
+                                withContext(Dispatchers.Default) {
+                                    updateR2Score()
+                                    println(BasicValues.getR2Score())
+                                    updateGradientIntercept()
+                                }
+                            }
+                            if (BasicValues.getR2Score()>=0.9){
+                                val intent =
+                                    Intent(this@CalibrationActivity, HomeActivity::class.java)
+                                startActivity(intent)
+                            }
                         }
+                        println("${BasicValues.getConcentrationDataArray()} : Concentration")
+                        println("${BasicValues.getVoltageDataArray()} : Voltage")
+                        counter.intValue = index + 1
                     }) {
                         Text(text = "Set Voltage")
                     }
@@ -90,22 +119,59 @@ class CalibrationActivity : ComponentActivity() {
             }
         }
 
-        Column (modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center ){
-            Text(text = "R-Square score: ${BasicValues.getR2Score()}", modifier=Modifier.padding(10.dp))
-            LazyVerticalGrid(columns = GridCells.FixedSize(165.dp), modifier = Modifier.padding(1.dp), verticalArrangement = Arrangement.Center, horizontalArrangement = Arrangement.Center) {
-                repeat(3) {
-                    item {
-                        CalibrateCard(index = it)
-                    }
-                }
-                while (counter.intValue>2 && counter.intValue<BasicValues.getCalibrationConcentration().size && BasicValues.getR2Score() < 0.9) {
-                    item {
-                        CalibrateCard(index = counter.intValue+1)
+        Column(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            val r2score = remember { mutableDoubleStateOf(0.0) }
+            val r2CoroutineScope = rememberCoroutineScope()
+            val additionalCards = remember { mutableIntStateOf(0) }
+            val shownAdditionalCards = remember { mutableIntStateOf(0) }
+
+            Text(
+                text = "R-Square score: ${r2score.doubleValue}",
+                modifier = Modifier.padding(10.dp)
+            )
+            LazyVerticalGrid(
+                columns = GridCells.FixedSize(165.dp),
+                modifier = Modifier.padding(1.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                repeat(BasicValues.getCalibrationConcentration().size) {
+                    val isVisible = it < 3 + shownAdditionalCards.intValue
+                    if (isVisible) {
+                        println("Showing Card number ${it + 1}")
+                        item {
+                            CalibrateCard(index = it)
+                        }
                     }
                 }
             }
-        }
 
+
+
+            r2CoroutineScope.launch {
+                withContext(Dispatchers.Default) {
+                    while (true) {
+                        r2score.doubleValue = BasicValues.getR2Score()
+                        println("\t\t THIS IS r2 ${r2score.doubleValue}")
+                        val isR2less =
+                            counter.intValue > 2 && counter.intValue < BasicValues.getCalibrationConcentration().size && r2score.doubleValue < 0.9
+                        println("\t is r2 less? $isR2less")
+                        if (isR2less && shownAdditionalCards.intValue < BasicValues.getCalibrationConcentration().size - 3) {
+                            shownAdditionalCards.intValue++ // Show one additional card
+                        }
+                        delay(10000) // Adjust delay between showing cards
+                    }
+                }
+            }
+
+
+        }
     }
 
     @Preview(showBackground = true)
