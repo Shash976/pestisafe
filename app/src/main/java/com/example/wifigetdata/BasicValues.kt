933d092a84ff6d27c17e2ad7b42a171b824ccf8b
Shashwat Goel <itshashgoel@gmail.com>
2024-05-22 12:36:16 +0530

    Changed value types to mutableState for ease of state maintenance

--- app/src/main/java/com/example/wifigetdata/BasicValues.kt
+++ app/src/main/java/com/example/wifigetdata/BasicValues.kt
@@ -1,32 +1,38 @@
 package com.example.wifigetdata
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import kotlinx.coroutines.flow.MutableStateFlow
 
 
 object BasicValues {
-    private var receivedVal = MutableStateFlow(0.0)
-    private var r2score :Double = 0.0
-    private var url = MutableStateFlow("")
-    private var calibrationConcentration = doubleArrayOf( 1.0, 10.0, 5.0, 7.5, 6.0, 2.5, 4.0, 1.25)
+    val receivedVal = MutableStateFlow(0.0)
+    private val r2score = mutableDoubleStateOf(0.0)
+    private val url = MutableStateFlow("")
+    private val calibrationConcentration = doubleArrayOf( 1.0, 10.0, 5.0, 7.5, 6.0, 2.5, 4.0, 1.25)
     private var voltageDataArray  = mutableListOf<Double>()
     private var concentrationDataArray = mutableListOf<Double>()
-    private var gradient :Double= 0.0
-    private var intercept :Double = 0.0
+    private val gradient : MutableState<Double> = mutableDoubleStateOf(0.0)
+    private val intercept = mutableDoubleStateOf(0.0)
+    private val updateTiming :MutableState<Long> = mutableLongStateOf(1500)
 
     fun getGradient() :Double {
-        return gradient
+        return gradient.value
     }
 
     fun getIntercept() :Double {
-        return intercept
+        return intercept.doubleValue
     }
 
     fun setGradient(newGradient: Double) {
-        gradient = newGradient
+        gradient.value = newGradient
     }
 
     fun setIntercept(newIntercept :Double) {
-        intercept = newIntercept
+        intercept.doubleValue = newIntercept
     }
 
     fun getCalibrationConcentration(): DoubleArray {
@@ -36,6 +42,22 @@ object BasicValues {
     fun updateData(newVoltage :Double, newConcentration :Double) {
         voltageDataArray.add(newVoltage)
         concentrationDataArray.add(newConcentration)
+
+        var array1= concentrationDataArray
+        var array2 = voltageDataArray
+
+        val pairList = array1.zip(array2).toList()
+        val sortedPairList = pairList.sortedBy { it.first }
+
+        array1 = sortedPairList.map { it.first }.toMutableList()
+        array2 = sortedPairList.map { it.second }.toMutableList()
+
+        voltageDataArray = array2
+        concentrationDataArray = array1
+
+        if (voltageDataArray.size > 2) {
+            updateGradientIntercept()
+        }
     }
 
     fun getVoltageDataArray() :MutableList<Double> {
@@ -61,9 +83,13 @@ object BasicValues {
         receivedVal.value = value
     }
     fun getR2Score() :Double{
-        return r2score
+        return r2score.doubleValue
     }
     fun setR2Score(value: Double){
-        r2score = value
+        r2score.doubleValue = value
+    }
+
+    fun getUpdateTiming() :Long {
+        return updateTiming.value
     }
 }
