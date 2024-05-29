3740e9eb0ce4a082eadac24aeb78728598c89ac7
Shashwat Goel <itshashgoel@gmail.com>
2024-05-29 11:25:53 +0530

    Added Navigation Graph

--- app/src/main/java/com/example/wifigetdata/MainActivity.kt
+++ app/src/main/java/com/example/wifigetdata/MainActivity.kt
@@ -1,77 +1,71 @@
 package com.example.wifigetdata
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import com.example.wifigetdata.ui.theme.WifigetdataTheme
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URI
-import java.net.URL
-import android.text.format.Formatter
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.coroutineScope
-import java.net.InetAddress
-import kotlin.math.sqrt
 
 
 open class MainActivity : ComponentActivity() {
-//    val database by lazy {
-//        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
-//    }
-
-    @SuppressLint("CoroutineCreationDuringComposition")
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "dataValues.db"
+        ).build()
+    }
+   // val sharedViewModel = ViewModelProvider((applicationContext as AppViewModel))[MainViewModel::class.java]
+    private lateinit var sharedViewModel : MainViewModel
+    private lateinit var repository : Repository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO){
+                repository = Repository(database.dataValueDao())
+                sharedViewModel = ViewModelProvider(
+                    this@MainActivity,
+                    MainViewModelFactory(repository)
+                )[MainViewModel::class.java]
+            }
+        }
         setContent {
             //val databaseDataValues by database.dataValueDao().getAll().observeAsState(emptyList())
-//            val (receivedVal, setReceivedVal) = remember { mutableDoubleStateOf(0.0) }
-//            var stringURL by remember {
-//                mutableStateOf("")
-//            }
             WifigetdataTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   val intent = Intent(this@MainActivity, SetURLActivity::class.java)
-                    startActivity(intent)
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Routes.MAIN.toString()){
+                        composable(Routes.MAIN.toString()){
+                            MainScreen(sharedViewModel, navController)
+                        }
+                        composable(Routes.IP_SCANNER.toString()){
+                            ScanScreen(sharedViewModel = sharedViewModel,
+                                applicationContext = this@MainActivity,
+                                navController = navController
+                            )
+                        }
+                        composable(Routes.CALIBRATION.toString()){
+                            CalibrationScreen(sharedViewModel = sharedViewModel, navController=navController)
+                        }
+                        composable(Routes.HOME.toString()){
+                            HomeScreen(sharedViewModel = sharedViewModel, navController = navController, context = this@MainActivity)
+                        }
+                    }
                 }
             }
         }
